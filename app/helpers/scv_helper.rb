c0deace2398d9cf7be7a7f8c54a01104eb60fd48
Benjamin Armintor <armintor@gmail.com>
2011-12-06 09:29:00 -0500

    jp2 resource helpers and index partials fixed for new format

--- app/helpers/scv_helper.rb
+++ app/helpers/scv_helper.rb
@@ -68,6 +68,55 @@ module ScvHelper
     end
   end
 
+  def basic_resource(document)
+    res = {}
+    res[:mime_type] = document["dc_format_t"].first
+    res[:content_models] = document["has_model_s"]
+    res[:file_size] = document["extent_s"].first.to_i
+    res[:size] = (document["extent_s"].first.to_i / 1024).to_s + " Kb"
+    res
+  end
+
+  def image_resource(document)
+    res = basic_resource(document)
+    if document["image_width_s"]
+      res[:dimensions] = document["image_width_s"].first + " x " + document["image_length_s"].first
+      res[:width] = document["image_width_s"].first
+      res[:height] = document["image_length_s"].first
+    else
+      res[:dimensions] = "? x ?"
+      res[:width] = "0"
+      res[:height] = "0"
+    end
+    base_id = document["id"]
+    base_filename = base_id.gsub(/\:/,"")
+    img_filename = base_filename + "." + document["dc_format_t"].first.gsub(/^[^\/]+\//,"")
+    dc_filename = base_filename + "_dc.xml"
+    res[:show_path] = fedora_content_path("show", base_id, "CONTENT", img_filename)
+    res[:cache_path] = cache_path("show", base_id, "CONTENT", img_filename)
+    res[:download_path] = fedora_content_path("download", base_id, "CONTENT", img_filename)
+    res[:dc_path] = fedora_content_path('show_pretty', base_id, "DC", dc_filename)
+    res
+  end
+
+  def audio_resource(document)
+    res = basic_resource(document)
+    base_id = document["id"]
+    base_filename = base_id.gsub(/\:/,"")
+    if res[:mime_type] =~ /wav/
+      ext = 'wav'
+    elsif res[:mime_type] =~ /mpeg/
+      ext = 'mp3'
+    else
+      ext = 'bin'
+    end
+    filename = base_filename + "." + ext
+    dc_filename = base_filename + "_dc.xml"
+    res[:download_path] = fedora_content_path("download", base_id, "CONTENT", filename)
+    res[:dc_path] = fedora_content_path('show_pretty', base_id, "DC", dc_filename)
+    res
+  end
+
   def get_resources(document)
     members = get_members(document)
     members = members.delete_if {|x| not x[:has_model_s].include? "info:fedora/ldpd:Resource" }
@@ -77,6 +126,7 @@ module ScvHelper
     format = format.first if format.is_a? Array
     case format
     when "zoomingimage"
+      results = members.collect {|doc| image_resource(doc)}
       base_id = base_id_for(document)
       url = Cul::Fedora::ResourceIndex.config[:riurl] + "/get/" + base_id + "/SOURCE"
       head_req = http_client.head(url)
@@ -84,59 +134,10 @@ module ScvHelper
       file_size = head_req.header["Content-Length"].first.to_i
       results << {:dimensions => "Original", :mime_type => "image/jp2", :show_path => fedora_content_path("show", base_id, "SOURCE", base_id + "_source.jp2"), :download_path => fedora_content_path("download", base_id , "SOURCE", base_id + "_source.jp2"), :content_models=>[]}  
     when "audio"
-      members.each do |member|
-        res = {}
-        res[:mime_type] = member["dc_format_t"].first
-        res[:content_models] = member["has_model_s"]
-        res[:file_size] = member["extent_s"].first.to_i
-        res[:size] = (member["extent_s"].first.to_i / 1024).to_s + " Kb"
-        base_id = member["id"]
-        base_filename = base_id.gsub(/\:/,"")
-        if res[:mime_type] =~ /wav/
-          ext = 'wav'
-        elsif res[:mime_type] =~ /mpeg/
-          ext = 'mp3'
-        else
-          ext = 'bin'
-        end
-        filename = base_filename + "." + ext
-        dc_filename = base_filename + "_dc.xml"
-
-        #res[:show_path] = fedora_content_path("show", base_id, "CONTENT", filename)
-        #res[:cache_path] = cache_path("show", base_id, "CONTENT", filename)
-        res[:download_path] = fedora_content_path("download", base_id, "CONTENT", filename)
-        res[:dc_path] = fedora_content_path('show_pretty', base_id, "DC", dc_filename)
-        results << res
-      end
+      results = members.collect {|doc| audio_resource(doc)}
     when "image"
         #images = parse_image_resources!(document)
-      members.each do |member|
-        res = {}
-        if member["image_width_s"]
-          res[:dimensions] = member["image_width_s"].first + " x " + member["image_length_s"].first
-          res[:width] = member["image_width_s"].first
-          res[:height] = member["image_length_s"].first
-        else
-          res[:dimensions] = "? x ?"
-          res[:width] = "0"
-          res[:height] = "0"
-        end
-        res[:mime_type] = member["dc_format_t"].first
-        res[:content_models] = member["has_model_s"]
-        res[:file_size] = member["extent_s"].first.to_i
-        res[:size] = (member["extent_s"].first.to_i / 1024).to_s + " Kb"
-
-        base_id = member["id"]
-        base_filename = base_id.gsub(/\:/,"")
-        img_filename = base_filename + "." + member["dc_format_t"].first.gsub(/^[^\/]+\//,"")
-        dc_filename = base_filename + "_dc.xml"
-
-        res[:show_path] = fedora_content_path("show", base_id, "CONTENT", img_filename)
-        res[:cache_path] = cache_path("show", base_id, "CONTENT", img_filename)
-        res[:download_path] = fedora_content_path("download", base_id, "CONTENT", img_filename)
-        res[:dc_path] = fedora_content_path('show_pretty', base_id, "DC", dc_filename)
-        results << res
-      end 
+      results = members.collect {|doc| image_resource(doc)}
     else
       raise "Unknown format #{format}"
     end
