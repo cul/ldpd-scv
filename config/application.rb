f838f4fd273a4628f05e3233455dc5f8beac03c1
Benjamin Armintor <armintor@gmail.com>
2013-05-21 15:45:32 -0400

    change application's env-specific loading

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  Bundler.require *Rails.groups(:assets => %w(development test))
+end
 require 'active_fedora'
 require 'cul_scv_hydra/engine'
 ActiveFedora::Datastream.send :include, ActiveFedora::Streamable::Datastreams
