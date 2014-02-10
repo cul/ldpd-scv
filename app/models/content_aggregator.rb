d535de38074c67890a296324347b971ddfa76df3
Benjamin Armintor <armintor@gmail.com>
2014-02-10 13:18:49 -0500

    SCV-127: Thumbnails should be for recto image

--- app/models/content_aggregator.rb
+++ app/models/content_aggregator.rb
@@ -14,15 +14,54 @@ class ContentAggregator < ::ActiveFedora::Base
   end
   
   def thumbnail_info
-    members = part_ids
+    r = self.parts(:response_format => :solr)
+    members = r.collect {|hit| SolrDocument.new(hit) } unless r.blank?
     if members.length == 0
       return {:asset=>"crystal/file.png",:mime=>'image/png'}
     else
-      member = ActiveFedora::Base.find(members[0], :cast=>true)
-      if member.respond_to? :thumbnail_info
-        return member.thumbnail_info
+      thumb = nil
+      unless datastreams['structMetadata'].new?
+        thumb = thumb_from_struct(members)
+      else
+        thumb =  thumb_from_members(members)
       end
     end
-    return {:asset=>"crystal/file.png",:mime=>'image/png'}
+    return thumb || {:asset=>"crystal/file.png",:mime=>'image/png'}
+  end
+
+  private
+  def thumb_from_struct(members)
+    sm = datastreams['structMetadata']
+    first = sm.divs_with_attribute(false,'ORDER','1').first
+    if first
+      members.each do |member|
+        if member["identifier_ssim"].include? first["CONTENTIDS"]
+          return thumb_from_solr_doc(member)
+        end
+      end
+      return nil
+    else
+      return nil
+    end
+  end
+
+  def thumb_from_members(members)
+    sorted = members.sort do |a,b|
+      c = a.title_si <=> b.title_si
+      return c unless c == 0
+      a.identifier_ssim.delete(a.id) unless a.identifier_ssim.length == 1
+      b.identifier_ssim.delete(a.id) unless b.identifier_ssim.length == 1
+      c = a.identifier_ssim[0] <=> b.identifier_ssim[0]
+      return c
+    end
+    thumb_from_solr_doc(sorted[0])
+  end
+
+  def thumb_from_solr_doc(solr_doc)
+    if solr_doc and (member =  ActiveFedora::Base.find(solr_doc.id, :cast=>true)).respond_to? :thumbnail_info
+      return member.thumbnail_info
+    else
+      return nil
+    end
   end
 end
