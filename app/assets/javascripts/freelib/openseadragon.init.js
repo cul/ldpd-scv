2697c30de32294c68d0aee562af5c381f89bb0e1
Benjamin Armintor <armintor@gmail.com>
2014-10-22 19:30:11 -0400

    move to repo-cache, not yet configurable

--- app/assets/javascripts/freelib/openseadragon.init.js
+++
@@ -1,6 +0,0 @@
-function init_seadragon(_rft, _showNav) {
-  var ts = new OpenSeadragon.DjTileSource("http://iris.cul.columbia.edu:8888/view/", _rft);
-  var viewer = new OpenSeadragon.Viewer("map", undefined, '/assets/seadragon/');
-  viewer.showNavigator = _showNav;
-  viewer.openTileSource(ts);
-}
--- app/assets/javascripts/zooming_image.js
+++
@@ -1,8 +0,0 @@
-//= require ./freelib/openseadragon
-//= require ./freelib/djtilesource
-//= require ./freelib/openseadragon.init
-//= require ./freelib/purl
-//#= require ./freelib/jquery.colorbox-min
-//= require ./freelib/jquery.tiny-draggable.min
-//= require ./freelib/jquery.nicescroll.min
-//= require ./freelib/jquery.bgswitcher
