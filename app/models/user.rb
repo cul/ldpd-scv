a9c2d9e3c19df020ef0f195cc5900ab29a2801ab
Benjamin Armintor <armintor@gmail.com>
2016-03-03 17:07:20 -0500

    noop password setter on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-require 'securerandom'
 class User < ActiveRecord::Base
 # Connects this user object to Blacklights Bookmarks and Folders. 
   include Blacklight::User
@@ -17,8 +16,8 @@ class User < ActiveRecord::Base
   end
 
   def set_personal_info_via_ldap
-    if wind_login
-      entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", wind_login)) || []
+    if uid
+      entry = Net::LDAP.new({:host => "ldap.columbia.edu", :port => 389}).search(:base => "o=Columbia University, c=US", :filter => Net::LDAP::Filter.eq("uid", uid)) || []
       entry = entry.first
 
       if entry
@@ -26,7 +25,7 @@ class User < ActiveRecord::Base
         if _mail.length > 6 and _mail.match(/^[\w.]+[@][\w.]+$/)
           self.email = _mail
         else
-          self.email = wind_login + '@columbia.edu'
+          self.email = uid + '@columbia.edu'
         end
         self.last_name = entry[:sn].to_s
         self.first_name = entry[:givenname].to_s
@@ -35,30 +34,39 @@ class User < ActiveRecord::Base
 
     return self
   end
+
   def password
-    SecureRandom.hex[0...20]
+    Devise.friendly_token[0,20]
   end
+
+  def password=(*val)
+    # NOOP
+  end
+
   def self.set_staff!(unis = [])
     unis.each do |uni|
-      if (u = User.find_by_login(uni))
+      if (u = User.find(uid: uni))
         u.update_attributes(:email => uni + "@columbia.edu", :cul_staff => true)
       else
-        User.create!(:login => uni, :wind_login => uni, :email => uni + "@columbia.edu", :cul_staff => true, :password => SecureRandom.base64(8)) 
+        u = User.create!(:uid => uni, :wind_login => uni, :email => uni + "@columbia.edu", :cul_staff => true, :password => SecureRandom.base64(8))
       end
     end
   end
+
   def self.unset_staff!(unis = [])
     unis.each do |uni|
-      if (u = User.find_by_login(uni))
+      if (u = User.find(uid: uni))
         u.update_attributes(:email => uni + "@columbia.edu", :cul_staff => false)
       else
-        User.create!(:login => uni, :wind_login => uni, :email => uni + "@columbia.edu", :cul_staff => false, :password => SecureRandom.base64(8)) 
+        u = User.create!(:uid => uni, :wind_login => uni, :email => uni + "@columbia.edu", :cul_staff => false, :password => SecureRandom.base64(8))
       end
     end
   end
+
   def role_symbols
     self.roles.collect {|r| r.to_sym}
   end
+
   def role? role_sym
     return true if role_sym.eql? :*
     role_symbols.detect {|sym| (sym.eql? role_sym.to_sym) || Role.role(sym).include?(role_sym)}
